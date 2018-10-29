f22b8ac64aa81f5a016d5399e7a9d07307f70970
s1792988 <j.inca@sms.ed.ac.uk>
2018-10-29 14:03:12 +0000

    Fixing the function of greetings

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello world again from Edinburgh!")
+def greetings(username, greeting):
+    print("Hello %s  from Edinburgh!, Good news! %s"%(username, greeting))
+
+
+#prints - "Hello User Test from Edinburgh!, I wish you a great year!"
+greetings("User Test", "you have passed the test!")
